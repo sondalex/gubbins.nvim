963603d890eceecc7dc68fc3cb9bbc72ab78ad27
Alexandre Sonderegger <61547150+sondalex@users.noreply.github.com>
2024-02-12 18:37:38 +0100

    Set indent to four spaces

--- lua/gubbins/utils.lua
+++ lua/gubbins/utils.lua
@@ -7,26 +7,26 @@ local split = vim.split
 M.nvim_err = vim.api.nvim_err_writeln
 
 function M.split(line, sep)
-	return split(line, sep)
+    return split(line, sep)
 end
 
 function M.splitlines(lines)
-	local res = {}
-	for _, line in ipairs(lines) do
-		for _, v2 in ipairs(M.split(line, "\n")) do
-			table.insert(res, v2)
-		end
-	end
-	return res
+    local res = {}
+    for _, line in ipairs(lines) do
+        for _, v2 in ipairs(M.split(line, "\n")) do
+            table.insert(res, v2)
+        end
+    end
+    return res
 end
 
 --- @param opts table
 function M.copy(opts)
-	local res = {}
-	for k, v in pairs(opts) do
-		res[k] = v
-	end
-	return res
+    local res = {}
+    for k, v in pairs(opts) do
+        res[k] = v
+    end
+    return res
 end
 
 --- @param tbl table
@@ -37,15 +37,15 @@ end
 ---   slice({"ls", "-l", "-h"}, 2, nil) => {"-l", "-h"}
 ---   ```
 function M.slice(tbl, start, stop)
-	local res = {}
-	local slice_end = stop
-	if stop == nil then
-		slice_end = #tbl
-	end
-	for i = start, slice_end do
-		table.insert(res, tbl[i])
-	end
-	return res
+    local res = {}
+    local slice_end = stop
+    if stop == nil then
+        slice_end = #tbl
+    end
+    for i = start, slice_end do
+        table.insert(res, tbl[i])
+    end
+    return res
 end
 
 return M
